76824a61d8a743fbc8588cd3ba8eee5482c03e4b
Alejo <alejo_g.p@hotmail.com>
2022-09-20 19:59:57 -0300

    Subir rama a GH

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'ReFocus',
+      title: 'ReFocus App',
       home: SelectRoute(),
     );
   }
