a366becba67f1506902166225911ea34f86fb51c
Alejo Pignol <alejo_g.p@hotmail.com>
2022-10-27 17:30:48 -0300

    Nueva visualización de la ruta. Incluido navegador

--- lib/widgets/route_element.dart
+++ lib/widgets/route_element.dart
@@ -15,49 +15,44 @@ class RouteElement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Card(
-        margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-        elevation: 10,
-        child: Container(
-            /* decoration: BoxDecoration(
+      child: Container(
+          decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               border: Border.all(),
-              /* boxShadow: const [
+              boxShadow: const [
                 BoxShadow(
                   blurStyle: BlurStyle.outer,
                   blurRadius: 15,
                 )
-              ], */
-            ), */
-            width: double.infinity,
-            //margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-            padding: const EdgeInsets.all(5),
-            //height: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  titulo,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      color: Color.fromRGBO(21, 178, 150, 100),
-                      fontFamily: 'Roboto'),
-                ),
-                Text(
-                  subtitulo,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                ),
-                Expanded(
-                    child: Image.network(
-                  imagen,
-                ))
-              ],
-            )),
-      ),
+              ]),
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          padding: const EdgeInsets.all(5),
+          //height: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                titulo,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                    color: Color.fromRGBO(21, 178, 150, 100),
+                    fontFamily: 'Roboto'),
+              ),
+              Text(
+                subtitulo,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
+              Expanded(
+                  child: Image.network(
+                imagen,
+              ))
+            ],
+          )),
     );
   }
 }
