95b3e6661e5cf68a6d6373099155be641072505d
Alejo <alejo_g.p@hotmail.com>
2023-05-11 19:42:19 -0300

    Boton de cerrar sesión en "Profile"

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,12 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({ Key? key }) : super(key: key);
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  singOut() async {
+    await _auth.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Perfil'),
-    );
+    return Center(
+        child: ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Icon(Icons.logout),
+                SizedBox(
+                  width: 8,
+                ),
+                Text("Cerrar sesión"),
+              ],
+            )));
   }
 }
