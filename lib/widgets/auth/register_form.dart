84d25b58eeae44dd3494f30267662e75e65412ae
Franco Garcia <francotgs@gmail.com>
2024-03-18 06:05:13 -0300

    responsive register form

--- lib/widgets/auth/register_form.dart
+++ lib/widgets/auth/register_form.dart
@@ -33,135 +33,142 @@ class _RegisterFormState extends State<RegisterForm> {
   @override
   Widget build(BuildContext context) {
     var formKey = widget.formKey;
-    return Center(
-      child: Card(
-        margin: const EdgeInsets.all(20),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextFormField(
-                    key: const ValueKey('email'),
-                    autocorrect: false,
-                    textCapitalization: TextCapitalization.none,
-                    enableSuggestions: false,
-                    validator: (value) {
-                      if (value == null ||
-                          value.isEmpty ||
-                          !value.contains('@')) {
-                        return 'Por favor ingrese un mail válido';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      labelText: 'Correo electrónico',
+    return Column(
+      children: [
+        Expanded(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Card(
+                margin: const EdgeInsets.all(20),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextFormField(
+                          key: const ValueKey('email'),
+                          autocorrect: false,
+                          textCapitalization: TextCapitalization.none,
+                          enableSuggestions: false,
+                          validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                !value.contains('@')) {
+                              return 'Por favor ingrese un mail válido';
+                            }
+                            return null;
+                          },
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            labelText: 'Correo electrónico',
+                          ),
+                          onSaved: (value) {
+                            _userEmail = value!;
+                            widget.setMail(_userEmail);
+                          },
+                        ),
+                        TextFormField(
+                          key: const ValueKey('username'),
+                          autocorrect: true,
+                          textCapitalization: TextCapitalization.words,
+                          enableSuggestions: false,
+                          validator: (value) {
+                            if (value == null || value.isEmpty || value.length < 4) {
+                              return 'El nombre de usuario debe tener al menos 4 caracteres';
+                            }
+                            return null;
+                          },
+                          decoration:
+                              const InputDecoration(labelText: 'Nombre de usuario'),
+                          onSaved: (value) {
+                            _userName = value!;
+                            widget.setUser(_userName);
+                          },
+                        ),
+                        TextFormField(
+                          key: const ValueKey('password'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty || value.length < 6) {
+                              return 'La contraseña debe tener al menos 6 caracteres';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(labelText: 'Contraseña'),
+                          obscureText: true,
+                          onSaved: (value) {
+                            _userPassword = value!;
+                            widget.setPassword(_userPassword);
+                          },
+                        ),
+                        TextFormField(
+                          key: const ValueKey('age'),
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, ingrese su edad';
+                            }
+                            int? age = int.tryParse(value);
+                            if (age == null || age <= 0) {
+                              return 'La edad debe ser un número positivo';
+                            } else if (age < 18 || age > 120) {
+                              return 'La aplicación es para mayores de 18 años';
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(labelText: 'Edad'),
+                          onSaved: (value) {
+                            _userAge = int.parse(value!);
+                            widget.setAge(_userAge);
+                          },
+                        ),
+                        DropdownButtonFormField(
+                          //value: _userGender,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _userGender = newValue.toString();
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Género',
+                            hintText: 'Selecciona tu género',
+                          ),
+                          items: ['Masculino', 'Femenino', 'No binario', 'Otro']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, selecciona tu género';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            _userGender = value!;
+                            widget.setGender(_userGender);
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                      ],
                     ),
-                    onSaved: (value) {
-                      _userEmail = value!;
-                      widget.setMail(_userEmail);
-                    },
                   ),
-                  TextFormField(
-                    key: const ValueKey('username'),
-                    autocorrect: true,
-                    textCapitalization: TextCapitalization.words,
-                    enableSuggestions: false,
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length < 4) {
-                        return 'El nombre de usuario debe tener al menos 4 caracteres';
-                      }
-                      return null;
-                    },
-                    decoration:
-                        const InputDecoration(labelText: 'Nombre de usuario'),
-                    onSaved: (value) {
-                      _userName = value!;
-                      widget.setUser(_userName);
-                    },
-                  ),
-                  TextFormField(
-                    key: const ValueKey('password'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length < 6) {
-                        return 'La contraseña debe tener al menos 6 caracteres';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(labelText: 'Contraseña'),
-                    obscureText: true,
-                    onSaved: (value) {
-                      _userPassword = value!;
-                      widget.setPassword(_userPassword);
-                    },
-                  ),
-                  TextFormField(
-                    key: const ValueKey('age'),
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, ingrese su edad';
-                      }
-                      int? age = int.tryParse(value);
-                      if (age == null || age <= 0) {
-                        return 'La edad debe ser un número positivo';
-                      } else if (age < 18 || age > 120) {
-                        return 'La aplicación es para mayores de 18 años';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(labelText: 'Edad'),
-                    onSaved: (value) {
-                      _userAge = int.parse(value!);
-                      widget.setAge(_userAge);
-                    },
-                  ),
-                  DropdownButtonFormField(
-                    //value: _userGender,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _userGender = newValue.toString();
-                      });
-                    },
-                    decoration: const InputDecoration(
-                      labelText: 'Género',
-                      hintText: 'Selecciona tu género',
-                    ),
-                    items: ['Masculino', 'Femenino', 'No binario', 'Otro']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor, selecciona tu género';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) {
-                      _userGender = value!;
-                      widget.setGender(_userGender);
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                ],
+                ),
               ),
             ),
           ),
         ),
-      ),
+        const SizedBox(height: 180),
+      ],
     );
   }
 }
